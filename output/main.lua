5e27a53881b1913fef23ed7bf7c643e7454c2e1a
Kevin Glanville <kevin.m.glanville@gmail.com>
2021-05-20 06:17:47 -0600

    Add output files to upload to device
    Add code files and web UI files to upload to a device, including an
    example main.lua

---
+++ output/main.lua
@@ -0,0 +1,20 @@
+local dataSetName = 'tslData'
+-- Sensor-specific read function
+local function read()
+    local val, err = tsl2561.getlux()
+
+    local errMsg = nil
+    if err ~= tsl2561.TSL2561_OK then
+        errMsg = err
+    end
+
+    return val, 'lux', errMsg
+end
+
+-- Sensor-specific initialization
+local tslOk = tsl2561.init(4, 5) == tsl2561.TSL2561_OK
+
+print('tls2561 status is ok: ', tslOk)
+
+-- Register the read function. We'll take it from here.
+node.LFS.registerReader(20000, read, dataSetName)
